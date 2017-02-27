261f9276ac51c579d6729ef5280e2b1c41f2c696
Richard Dalton <richard@devjoy.com>
2017-02-27 07:21:24 +0000

    Initial Commit

---
+++ manage.py
@@ -0,0 +1,6 @@
+import sys
+from django.core.management import execute_from_command_line
+import settings
+
+if __name__ == "__main__":
+    execute_from_command_line(sys.argv)
---
+++ settings.py
@@ -0,0 +1,13 @@
+from urls import urlpatterns
+from django.conf import settings
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='thisisthesecretkey',
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
